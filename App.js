fddf7fb4546a7f441b18afe9fd9a9eb2eeceff62
Will Parreiras <williamparreiras10@gmail.com>
2025-03-18 20:25:57 -0300

    Primeira Aula

--- App.js
+++ App.js
@@ -1,19 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
+
+  console.disableYellowbox = true;
+
+  const [segundos,setarSegundos] = useState(0);
+  const [minutos,setarMinutos] = useState(0);
+
+  const [alarmeSound,setarAlarmSound] = ([
+    {
+      selecionado: true,
+      som:'alarme 1',
+      file: 'alarme1.mp3'
+    },
+
+    {
+      selecionado: false,
+      som: 'alarme 2',
+      file: 'alarme2.mp3'
+    }
+  ])
+
+  var numeros = [];
+  for(var i = 1; i<=60; i++){
+      numeros.push(i)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: "white", fontSize: 30}}>Selecione o seu Tempo:</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{color: 'white'}}>Min:</Text>
+          <Picker 
+          selectedValue={minutos}
+          onValueChange={(itemValue, itemIndex) => setarMinutos(itemValue)}
+          style={{ height: 60, width: 100, color: 'white'}}>
+            {
+            numeros.map(function(val){
+              return(<Picker.Item label={val.toString()} value={val.toString()} />);
+            })
+            }
+
+          </Picker>
+          <Text style={{color: 'white'}}>Seg:</Text>
+          <Picker 
+          selectedValue={segundos}
+          onValueChange={(itemValue, itemIndex) => setarSegundos(itemValue)}
+          style={{ height: 60, width: 100, color: 'white'}}>
+          <Picker.Item label="0" value="0" />
+            {
+            numeros.map(function(val){
+              return(<Picker.Item label={val.toString()} value={val.toString()} />);
+            })
+            } 
+          </Picker>
+        <StatusBar style="auto" />
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0FCCD6',
     alignItems: 'center',
     justifyContent: 'center',
   },
